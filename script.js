f4705b50dbd30a834ec5bf207b7023bf3a4a5ece
olapeju <damilola.olapeju@gmail.com>
2019-03-14 13:01:21 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const Swal = require('sweetalert2');
+
+function happy() {
+    Swal({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        type: 'error',
+        confirmButtonText: 'Cool'
+      })
+      
+
+    // var span = document.createElement("span");
+    // span.innerHTML='abc<br>efg'
+    // swal({
+    //     title: 'Select your actual mood',
+    //     content: {
+    //         element: "input",
+    //         attributes: {
+    //           //placeholder: "Type your password",
+    //           type: "radio",
+    //           value: "party"
+    //         },
+    //       },
+    //   title: 'Título',
+    //   text: 'Mensaje <br> de texto',
+    //   html: '<p>Mensaje de texto con <strong>formato</strong>.</p>',
+    //   type: 'success',
+      
+    //   timer: 3000,
+    // });
+  }
+
+  
